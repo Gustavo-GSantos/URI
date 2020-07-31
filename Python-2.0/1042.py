d2ef3df64d97ab0d3bdf09fd636cfb44ae374fd7
Gustavo Gonçalves Dos Santos <gustavogds9@hotmail.com>
2020-07-31 15:59:15 -0300

    Add 0.1

--- Python-2.0/1042.py
+++ Python-2.0/1042.py
@@ -1,4 +1,13 @@
-n1 = 2
-n2 = 3
-r = n1 + n2
-print(r)
+nums = input().split()
+a, b, c = nums
+lista = []
+lista.append(int(a))
+lista.append(int(b))
+lista.append(int(c))
+s = sorted(lista)
+
+for i in range(3):
+    print(s[i])
+print()
+for i2 in range(3):
+    print(lista[i2])
